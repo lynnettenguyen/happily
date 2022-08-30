5745f64173c1a87a3b304172cc48754af7af2fa0
lynnettenguyen <nguyelynne@gmail.com>
2022-08-30 13:15:33 -0700

    signup validations

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,27 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Regexp, Length
 from app.models import User
 
 
 def user_exists(form, field):
-    # Checking if user exists
+    # checking if user exists
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
-        raise ValidationError('Email address is already in use.')
+        raise ValidationError('Email address already in use')
 
 
 def username_exists(form, field):
-    # Checking if username is already in use
+    # checking if username is already in use
     username = field.data
     user = User.query.filter(User.username == username).first()
     if user:
-        raise ValidationError('Username is already in use.')
+        raise ValidationError('Username is already in use')
 
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username', validators=[DataRequired(), username_exists, Length=(min=4, message='Please provide a username at least 4 characters long')])
+    email = StringField('email', validators=[DataRequired(), user_exists, Email('Please provide a valid email address')])
+    password = StringField('password', validators=[DataRequired(), Regexp(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$', message='Passwords must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter, and 1 number.')])
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,13 +8,14 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import HomePage from './components/Home/HomePage';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -35,14 +36,14 @@ function App() {
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
+          <UsersList />
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+        <Route path='/'>
+          <HomePage />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
