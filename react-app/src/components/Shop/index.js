9f1e69742367762b28d7c8e2e696a491a1d73330
lynnettenguyen <nguyelynne@gmail.com>
2022-09-02 00:28:17 -0700

    working on shop image

--- react-app/src/components/Shop/index.js
+++ react-app/src/components/Shop/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useParams, useHistory } from "react-router-dom";
+import shopBackground from '../CSS/Images/new_shop.jpg'
+import '../CSS/Shop.css'
 
 const Shop = () => {
 
@@ -9,9 +11,23 @@ const Shop = () => {
 
 
   return (
-    <>
-      SHOP
-    </>
+    <div className='sell-product-main'>
+      <div className='new-shop-name-outer'>
+      <img src={shopBackground} className='shop-background-img'></img>
+        <div className='sell-product-main-header'>Millions of shopper can't wait to see what you have in store</div>
+      </div>
+      <div className='sell-product-bottom'>
+        <div>
+          <img src="https://img0.etsystatic.com/site-assets/sell-page/Sell_Page_Icon_Great_Value.svg" width="150" alt=""></img>
+        </div>
+        <div>
+          <img src="https://img0.etsystatic.com/site-assets/sell-page/Sell_Page_Icon_Tools.svg" width="150" alt=""></img>
+        </div>
+        <div>
+          <img src="https://img0.etsystatic.com/site-assets/sell-page/Sell_Page_Icon_Education.svg" width="150" alt=""></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
