f96f51c66783cef3d9130488af00c18c3192c764
lynnettenguyen <nguyelynne@gmail.com>
2022-09-04 00:33:39 -0700

    multiple photo upload

--- react-app/src/components/Shop/ImageUpload.js
+++ react-app/src/components/Shop/ImageUpload.js
@@ -8,10 +8,13 @@ const ImageUpload = ({ productId }) => {
   const history = useHistory();
   const dispatch = useDispatch();
   const [image, setImage] = useState(null);
+  const [image2, setImage2] = useState(null);
+  const [image3, setImage3] = useState(null);
+  const [image4, setImage4] = useState(null);
   const [imageLoading, setImageLoading] = useState(false);
   const user = useSelector(state => state.session.user)
+  const [errors, setErrors] = useState([])
 
-  // console.log(productId)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,8 +22,6 @@ const ImageUpload = ({ productId }) => {
     formData.append("image", image);
     formData.append("product_id", productId);
     formData.append("user_id", user.id)
-    // aws uploads can be a bit slow—displaying
-    // some sort of loading message is a good idea
 
     setImageLoading(true);
 
@@ -38,7 +39,7 @@ const ImageUpload = ({ productId }) => {
     }
     else {
       setImageLoading(false);
-      console.log("error");
+      setErrors(['Image is not a valid file type (.png, .jpeg, .jpg)'])
     }
   }
 
@@ -46,18 +47,53 @@ const ImageUpload = ({ productId }) => {
     const file = e.target.files[0];
     setImage(file);
   }
+  const updateImage2 = (e) => {
+    const file = e.target.files[0];
+    setImage2(file);
+  }
+  const updateImage3 = (e) => {
+    const file = e.target.files[0];
+    setImage3(file);
+  }
+  const updateImage4 = (e) => {
+    const file = e.target.files[0];
+    setImage4(file);
+  }
 
   return (<>
     <div>Photos</div>
     <form onSubmit={handleSubmit}>
       <input
         type="file"
-        accept=".png, .jpeg, .jpg, .gif, .webp"
+        accept="image/*"
         onChange={updateImage}
-        // style={{display: 'none'}}
       />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={updateImage2}
+      />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={updateImage3}
+      />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={updateImage4}
+      />
+      <img src={image ? URL.createObjectURL(image) : ""}></img>
+      <img src={image2 ? URL.createObjectURL(image2) : ""}></img>
+      <img src={image3 ? URL.createObjectURL(image3) : ""}></img>
+      <img src={image4 ? URL.createObjectURL(image4) : ""}></img>
       <button type="submit">Submit</button>
       {(imageLoading) && <p>Loading...</p>}
+      {errors?.map((error) => {
+        return (
+          <div>{error}</div>
+        )
+      })}
     </form>
   </>
   )
--- react-app/src/store/images.js
+++ react-app/src/store/images.js
@@ -1,26 +1,39 @@
 const LOAD_IMAGES = 'images/LOAD_IMAGES'
+const ADD_IMAGE = 'images/ADD_IMAGE'
+const DELETE_IMAGE = 'images/DELETE_IMAGE'
 
-const getCategories = (categories) => ({
+const loadImages = (images) => ({
   type: LOAD_IMAGES,
-  categories
+  images
 })
 
-export const getAllCategories = () => async (dispatch) => {
-  const response = await fetch(`/api/categories`);
+const addImages = (image) => ({
+  type: ADD_IMAGE,
+  image
+})
 
-  if (response.ok) {
-    const categories = await response.json();
-    dispatch(getCategories(categories))
-    return categories;
-  }
-}
+const deleteImages = (imageId) => ({
+  type: DELETE_IMAGE,
+  imageId
+})
 
 
-const imageReduce = (state = {}, action) => {
+
+const imageReducer = (state = {}, action) => {
   let newState = {}
   switch (action.type) {
     case LOAD_IMAGES: {
-      for (let category of action.categories) newState[category.id] = category
+      for (let image of action.images) newState[image.id] = image
+      return newState
+    }
+    case ADD_IMAGE: {
+      newState = { ...state }
+      newState[action.image.id] = action.image
+      return newState
+    }
+    case DELETE_IMAGE: {
+      newState = { ...state }
+      delete newState[action.image.id]
       return newState
     }
     default:
@@ -28,4 +41,4 @@ const imageReduce = (state = {}, action) => {
   }
 }
 
-export default imageReduce
+export default imageReducer
