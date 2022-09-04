9cc8e78b217cf2c56fe369585a1408dfb0d8aeef
lynnettenguyen <nguyelynne@gmail.com>
2022-09-04 12:45:09 -0700

    edited seed data

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -16,11 +16,11 @@ def seed_reviews():
     Review(
         user_id=5, product_id=2, content='Fast shipping and item just as described. Happy with the purchase!', stars=5),
     Review(
-        user_id=4, product_id=3, content='The poster looks great from a distance but up close you can see it’s a little blurry but the sellers did mention that it is up to you to make what you want look great and that they don\’t do any editing to it.', stars=3),
+        user_id=4, product_id=3, content='The poster looks great from a distance but up close you can see it’s a little blurry but the sellers did mention that it is up to you to make what you want look great and that they don’t do any editing to it.', stars=3),
     Review(
         user_id=5, product_id=3, content='These signs look fantastic, exactly what we were looking for for our farm style wedding. The seller was extremely quick to respond and helped us through some carrier problems we experienced.', stars=5),
     Review(
-        user_id=6, product_id=3, content='My decal wouldn\’t unstick from the backing so when trying to apply the sticker to my window the letters were all wrinkled and crooked. I wouldn\’t recommend nor will I order anything else. Shipping was slow also.', stars=1),
+        user_id=6, product_id=3, content='My decal wouldn’t unstick from the backing so when trying to apply the sticker to my window the letters were all wrinkled and crooked. I wouldn’t recommend nor will I order anything else. Shipping was slow also.', stars=1),
     Review(
         user_id=2, product_id=4, content='If looks exactly like the sample photos online and it is good quality for a great price!', stars=5),
     Review(
@@ -30,7 +30,37 @@ def seed_reviews():
     Review(
         user_id=5, product_id=5, content='The sign turned out even better than the picture. Absolutely love it!!!', stars=5),
     Review(
-        user_id=6, product_id=5, content='The sign was well made and came out beautiful! Shipping was fast too!', stars=5)
+        user_id=6, product_id=5, content='The sign was well made and came out beautiful! Shipping was fast too!', stars=5),
+    Review(
+        user_id=6, product_id=6, content='The item shipped so fast and was packaged SO good to the point it took me longer than I wanted to open it! I am so in love with everything about it. It’s going to be perfect as my welcome sign to the wedding! Thank you so much!', stars=5),
+    Review(
+        user_id=6, product_id=7, content='Fast and quick delivery! They showed us the product before they sent it to us to make sure it was correct. Simple and perfect without breaking your pockets. Thank you!!', stars=5),
+    Review(
+        user_id=8, product_id=8, content='We were able to include custom copy and the sign looks beautiful - it came quick and was packaged well. Can\'t wait to use this at our upcoming wedding!', stars=5),
+    Review(
+        user_id=6, product_id=9, content='SO BEAUTIFUL. Perfect for my wedding', stars=5),
+    Review(
+        user_id=7, product_id=10, content='Definitely a 10000/10000, it came on time and fast with shipping. The sign is definitely huge but that\'s how we would the sign to be', stars=5),
+    Review(
+        user_id=6, product_id=11, content='Absolutely gorgeous sign, and the turn-around time was fast! This will be a terrific addition to my daughter\'s wedding! <3', stars=4),
+    Review(
+        user_id=5, product_id=12, content='I absolutely love the seating chart, my fiancé was very excited for it too! It matches all of my other signs perfectly. Everything came out perfectly, plus it was shipped and delivered within a week!', stars=4),
+    Review(
+        user_id=4, product_id=13, content='This seller is hands down the best on Etsy! They are so patient, and understanding! I had a few corrections to my seating chart, and without hesitation they fixed it. I absolutely love my seating chart!!!', stars=4),
+    Review(
+        user_id=8, product_id=14, content='If I could give this 10/5 stars, I would. The sign was quality, beautiful, and PERFECT. ', stars=5),
+    Review(
+        user_id=1, product_id=15, content='Got so many compliments on how beautiful the chart was! It was perfect for our wedding. When there was an issue with shipping the vendor was extremely responsive and helpful. 10/10 would recommend.', stars=5),
+    Review(
+        user_id=3, product_id=16, content='I cannot stress enough how wonderful this shop is. Communication was wonderful and they were so fast making and shipping my seating chart.', stars=5),
+    Review(
+        user_id=8, product_id=17, content='I am blown away!! This seating chart is absolutely beautiful! Every single detail that the seller puts into it shows.', stars=5),
+    Review(
+        user_id=6, product_id=18, content='Wow. Absolutely perfect! 100% worth the money! It arrived two weeks early so we didn’t have to sweat if it would be here in time! Highly recommend.', stars=5),
+    Review(
+        user_id=7, product_id=19, content='The quality, the beauty, the value and overall look of this gorgeous seating chart sign is beyond my hopes or expectations.', stars=5),
+    Review(
+        user_id=5, product_id=20, content='This is perfect!! More than five stars!! So special and so beautiful! It’s amazing! Customer Service was great. It shipped quick! Everything was A+!! Thank you!', stars=5)
     ]
 
     for review in reviews:
@@ -39,8 +69,6 @@ def seed_reviews():
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE the reviews table because SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and RESET IDENTITY resets the auto incrementing primary key, CASCADE deletes any dependent entities
 def undo_reviews():
     db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
     db.session.commit()
--- react-app/src/components/Shop/ImageUpload.js
+++ react-app/src/components/Shop/ImageUpload.js
@@ -62,75 +62,76 @@ const ImageUpload = ({ productId }) => {
     setImage4(file);
   }
 
-  return (<div className="upload-image-main">
-    <div className="photo-header">Add Photos</div>
-    <div className="photo-caption">Use up to four photos to show your item's most important qualities.</div>
-    <form onSubmit={handleSubmit}>
-      <div className="photo-upload-main">
-        <div className="file-upload-outer">
-          <label for='file-upload' className="file-upload-label">
-            <img src={photo} className='file-upload-image'></img>Add a Photo
-          </label>
+  return (
+    <div className="upload-image-main">
+      <div className="photo-header">Add Photos</div>
+      <div className="photo-caption">Use up to four photos to show your item's most important qualities.</div>
+      <form onSubmit={handleSubmit}>
+        <div className="photo-upload-main">
+          <div className="file-upload-outer">
+            <label for='file-upload' className="file-upload-label">
+              <img src={photo} className='file-upload-image'></img>Add a Photo
+            </label>
+          </div>
+          <div className="file-upload-outer">
+            <label for='file-upload2' className="file-upload-label">
+              <img src={photo} className='file-upload-image'></img>Add a Photo
+            </label>
+          </div>
+          <div className="file-upload-outer">
+            <label for='file-upload3' className="file-upload-label">
+              <img src={photo} className='file-upload-image'></img>Add a Photo
+            </label>
+          </div>
+          <div className="file-upload-outer">
+            <label for='file-upload4' className="file-upload-label">
+              <img src={photo} className='file-upload-image'></img>Add a Photo
+            </label>
+          </div>
         </div>
-        <div className="file-upload-outer">
-          <label for='file-upload2' className="file-upload-label">
-            <img src={photo} className='file-upload-image'></img>Add a Photo
-          </label>
-        </div>
-        <div className="file-upload-outer">
-          <label for='file-upload3' className="file-upload-label">
-            <img src={photo} className='file-upload-image'></img>Add a Photo
-          </label>
-        </div>
-        <div className="file-upload-outer">
-          <label for='file-upload4' className="file-upload-label">
-            <img src={photo} className='file-upload-image'></img>Add a Photo
-          </label>
-        </div>
-      </div>
-      <input
-        id='file-upload'
-        type="file"
-        accept="image/*"
-        onChange={updateImage}
-        style={{ display: 'none' }}
-      />
-      <input
-        id='file-upload2'
-        type="file"
-        accept="image/*"
-        onChange={updateImage2}
-        style={{ display: 'none' }}
+        <input
+          id='file-upload'
+          type="file"
+          accept="image/*"
+          onChange={updateImage}
+          style={{ display: 'none' }}
         />
-      <input
-        id='file-upload3'
-        type="file"
-        accept="image/*"
-        onChange={updateImage3}
-        style={{ display: 'none' }}
-      />
-      <input
-        id='file-upload4'
-        type="file"
-        accept="image/*"
-        onChange={updateImage4}
-        style={{ display: 'none' }}
-      />
-      <div className="photo-display-preview">
-      <img src={image ? URL.createObjectURL(image) : ""} className='photo-preview'></img>
-      <img src={image2 ? URL.createObjectURL(image2) : ""} className='photo-preview'></img>
-      <img src={image3 ? URL.createObjectURL(image3) : ""} className='photo-preview'></img>
-      <img src={image4 ? URL.createObjectURL(image4) : ""} className='photo-preview'></img>
-      </div>
-      <button type="submit">Submit</button>
-      {(imageLoading) && <p>Loading...</p>}
-      {errors?.map((error) => {
-        return (
-          <div>{error}</div>
-        )
-      })}
-    </form>
-  </div>
+        <input
+          id='file-upload2'
+          type="file"
+          accept="image/*"
+          onChange={updateImage2}
+          style={{ display: 'none' }}
+        />
+        <input
+          id='file-upload3'
+          type="file"
+          accept="image/*"
+          onChange={updateImage3}
+          style={{ display: 'none' }}
+        />
+        <input
+          id='file-upload4'
+          type="file"
+          accept="image/*"
+          onChange={updateImage4}
+          style={{ display: 'none' }}
+        />
+        <div className="photo-display-preview">
+          <img src={image ? URL.createObjectURL(image) : ""} className='photo-preview'></img>
+          <img src={image2 ? URL.createObjectURL(image2) : ""} className='photo-preview'></img>
+          <img src={image3 ? URL.createObjectURL(image3) : ""} className='photo-preview'></img>
+          <img src={image4 ? URL.createObjectURL(image4) : ""} className='photo-preview'></img>
+        </div>
+        <button type="submit">Submit</button>
+        {(imageLoading) && <p>Loading...</p>}
+        {errors?.map((error) => {
+          return (
+            <div>{error}</div>
+          )
+        })}
+      </form>
+    </div>
   )
 }
 
