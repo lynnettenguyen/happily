6cc0fccc1548557721703eacfa87f9227fa67361
lynnettenguyen <nguyelynne@gmail.com>
2022-09-05 13:33:28 -0700

    purchase store

---
+++ react-app/src/store/purchases.js
@@ -0,0 +1,56 @@
+const GET_PURCHASES = 'purchases/GET_PURCHASES'
+const REMOVE_PURCHASE = 'purchases/REMOVE_PURCHASE'
+
+const getPurchases = (purchases) => ({
+  type: GET_PURCHASES,
+  purchases
+})
+
+const removePurchases = (orderNumber) => ({
+  type: REMOVE_PURCHASE,
+  orderNumber
+})
+
+
+export const getAllPurchases = (userId) => async (dispatch) => {
+  const response = await fetch(`/api/users/${userId}/purchases`);
+
+  if (response.ok) {
+    const purchases = await response.json();
+    dispatch(getPurchases(purchases))
+    return purchases;
+  }
+}
+
+
+export const cancelPurchase = (orderNumber) => async (dispatch) => {
+  const response = await fetch(`/api/purchases/${orderNumber}`, {
+    method: "DELETE"
+  })
+
+  if (response.ok) {
+    const purchase = await response.json();
+    dispatch(removePurchases(orderNumber))
+    return purchase;
+  }
+}
+
+
+const purchaseReducer = (state = {}, action) => {
+  let newState = {}
+  switch (action.type) {
+    case GET_PURCHASES: {
+      for (let purchase of action.purchases) newState[purchase.order_number.toUpperCase()] = purchase
+      return newState
+    }
+    case REMOVE_PURCHASE: {
+      newState = { ...state }
+      delete newState[action.orderNumber.toUpperCase()]
+      return newState
+    }
+    default:
+      return state;
+  }
+}
+
+export default purchaseReducer
